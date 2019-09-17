c5daa126b9df757edba182c9c36d9d2f2327344f
Sergio Comerón <sergiocomeron@icloud.com>
2019-09-17 11:17:03 +0200

    new version

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2019051600;
+$plugin->version   = 2019091700;
 $plugin->requires  = 2017050500;
 $plugin->component = 'block_common_courses';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '1.4';
+$plugin->release   = '1.5';
