a8d87efe1b6743357416e2a52863bcda0524c190
sergiocomeron <sergiocomeron@icloud.com>
2021-05-14 13:49:19 +0200

    Code corrections

--- block_common_courses.php
+++ block_common_courses.php
@@ -41,7 +41,7 @@ class block_common_courses extends block_list {
      *
      */
     public function applicable_formats() {
-        // Only add at user-profile and other course-profiles (weeks & topics)
+        // Only add at user-profile and other course-profiles (weeks & topics).
         return array('all' => true);
     }
 
